963b7105ee8c2c9e7edf19e2eb8794bc8cd6bac8
amuhongo <agostinhomuhongofeliciano@gmail.com>
2025-07-01 16:39:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+- `mysql_cli.py`:
+```python
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='seu_usuario',
+    password='sua_senha',
+    database='sua_base'
+)
+
+cursor = conn.cursor()
+consulta = input("Digite sua consulta SQL: ")
+cursor.execute(consulta)
+
+for resultado in cursor:
+    print(resultado)
+
+conn.close()
